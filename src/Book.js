ca6f0ff0fdd42fb495fd14a4525a49532439eace
Eric <ericmiller@umn.edu>
2018-09-15 10:19:39 -0500

    added Book and BooksView Components. Wired Books up to search. Working on wiring up BooksView

---
+++ src/Book.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+class Book extends React.Component {
+  render() {
+    const { item } = this.props
+
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${item.imageLinks.thumbnail})` }}></div>
+              <div className="book-shelf-changer">
+                <select>
+                  <option value="move" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <div className="book-title">{item.title}</div>
+            <div className="book-authors">{item.author}</div>
+        </div>
+      </li>
+    )
+  }
+}
+
+export default Book
